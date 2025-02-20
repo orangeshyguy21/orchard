a5ba36e5de27b4057ae9e49e622bbd3d3cfc38ac
Shyguy <shyguy@nodenebula.com>
2025-02-20 14:28:45 -0700

    rendering nav icons

--- src/client/app.component.ts
+++ src/client/app.component.ts
@@ -1,18 +1,17 @@
 /* Core Dependencies */
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-
+/* Vendor Dependencies */
 import { MatIconRegistry } from '@angular/material/icon';
 
-
 @Component({
-  selector: 'orc-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: false,
+	selector: 'orc-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	standalone: false,
 })
 export class AppComponent {
-  constructor(private matIconRegistry: MatIconRegistry) {
-    this.matIconRegistry.setDefaultFontSetClass('mat-symbol');
-  }
+	constructor(private matIconRegistry: MatIconRegistry) {
+		this.matIconRegistry.setDefaultFontSetClass('mat-symbol');
+	}
 }
