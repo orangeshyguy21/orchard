117c5e78323365fd94e6cb154bddf56959e40004
Shyguy <shyguy@nodenebula.com>
2025-03-19 13:30:15 -0600

    api federation
    make the api match the architecture of a client application a bit closer

--- src/server/modules/api/api.module.ts
+++ src/server/modules/api/api.module.ts
@@ -2,15 +2,16 @@
 import { Module } from "@nestjs/common";
 /* Local Dependencies */
 import { StatusModule } from "./status/status.module";
-import { MintInfoModule } from "./mintinfo/mintinfo.module";
-import { MintBalanceModule } from "./mintbalance/mintbalance.module";
-import { MintKeysetModule } from './mintkeyset/mintkeyset.module';
-import { MintDatabaseModule } from "./mintdatabase/mintdatabase.module";
-import { MintMeltQuoteModule } from "./mintmeltquote/mintmeltquote.module";
-import { MintMintQuoteModule } from "./mintmintquote/mintmintquote.module";
-import { MintPromiseModule } from "./mintpromise/mintpromise.module";
-import { MintProofModule } from "./mintproof/mintproof.module";
-import { MintAnalyticsModule } from "./mintanalytics/mintanalytics.module";
+import { MintInfoModule } from "./mint/info/mintinfo.module";
+import { MintBalanceModule } from "./mint/balance/mintbalance.module";
+import { MintKeysetModule } from './mint/keyset/mintkeyset.module';
+import { MintDatabaseModule } from "./mint/database/mintdatabase.module";
+import { MintMeltQuoteModule } from "./mint/meltquote/mintmeltquote.module";
+import { MintMintQuoteModule } from "./mint/mintquote/mintmintquote.module";
+import { MintPromiseModule } from "./mint/promise/mintpromise.module";
+import { MintProofModule } from "./mint/proof/mintproof.module";
+import { MintAnalyticsModule } from "./mint/analytics/mintanalytics.module";
+
 @Module({
   imports: [
     StatusModule,
