4b8236e29da5b2f47ee596db4c555d936729f76e
Shyguy <shyguy@nodenebula.com>
2025-04-02 20:47:43 -0600

    fix reserve ratio

--- src/client/modules/mint/components/mint-balance-sheet/mint-balance-row.class.ts
+++ src/client/modules/mint/components/mint-balance-sheet/mint-balance-row.class.ts
@@ -11,11 +11,16 @@ export class MintBalanceRow {
     liabilities: number;
     fee: number;
     active: boolean;
-    keyset_expiration: number|null;
     derivation_path: string;
     first_seen: number;
-    reserve_ratio: string;
     assets: number;
+
+    public get reserve_ratio(): string {
+        const ratio = this.assets / AmountPipe.getConvertedAmount(this.unit, this.liabilities);
+        const formatted_ratio = Number.isInteger(ratio) ? ratio.toString() : (ratio.toFixed(1).endsWith('.0') ? Math.floor(ratio).toString() : ratio.toFixed(1));
+        if (ratio === Infinity) return '∞ : 1';
+        return `${formatted_ratio} : 1`;
+    }
 
     constructor(balance: MintBalance | undefined, keyset: MintKeyset) {
         this.unit = keyset.unit;
@@ -25,8 +30,6 @@ export class MintBalanceRow {
         this.assets = this.tempSetAssets();
         this.derivation_path = keyset.derivation_path;
         this.first_seen = keyset.first_seen;
-        this.keyset_expiration = this.setKeysetExpiration(keyset);
-        this.reserve_ratio = this.setReserveRatio(this.assets); // @todo need ln data here
     }
 
     private tempSetAssets(): number {
@@ -34,17 +37,5 @@ export class MintBalanceRow {
         if(this.unit === 'usd') return 2500;
         if(this.unit === 'eur') return 1925;
         return 550000;
-    }
-
-    private setKeysetExpiration(keyset: MintKeyset): number|null {
-        if( keyset.valid_from === keyset.valid_to ) return null;
-        return keyset.valid_to;
-    }
-
-    private setReserveRatio(assets: number = 1000): string {
-        const ratio = assets / AmountPipe.getConvertedAmount(this.unit, this.liabilities);
-        const formatted_ratio = Number.isInteger(ratio) ? ratio.toString() : (ratio.toFixed(1).endsWith('.0') ? Math.floor(ratio).toString() : ratio.toFixed(1));
-        if (!isFinite(ratio))return '∞ : 1';
-        return `${formatted_ratio} : 1`;
     }
 }
--- src/client/modules/mint/components/mint-balance-sheet/mint-balance-sheet.component.ts
+++ src/client/modules/mint/components/mint-balance-sheet/mint-balance-sheet.component.ts
@@ -50,23 +50,6 @@ export class MintBalanceSheetComponent implements OnChanges {
 				}
 				rows_by_unit[unit].liabilities += row.liabilities;
 			});
-
-		// this.balances
-		// 	.map(balance => {
-		// 		const keyset = this.keysets.find(keyset => keyset.id === balance.keyset);
-		// 		if (!keyset) return null;
-		// 		return new MintBalanceRow(balance, keyset);
-		// 	})
-		// 	.filter(row => row !== null)
-		// 	.sort((a, b) => b.first_seen - a.first_seen)
-		// 	.forEach( row => {
-		// 		const unit = row.unit.toLowerCase();
-		// 		if (!rows_by_unit[unit]) {
-		// 			rows_by_unit[unit] = row;
-		// 			return;
-		// 		}
-		// 		rows_by_unit[unit].liabilities += row.liabilities;
-		// 	});
 		
 		return Object.values(rows_by_unit).sort((a, b) => {
 			const currency_order: Record<string, number> = { 'btc': 1, 'sat': 2, 'msat': 3, 'usd': 4, 'eur': 5 };
