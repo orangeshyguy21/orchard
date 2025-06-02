e7ff71f40bbd4a94513ecde68d2d84d2b6fbee67
Shyguy <shyguy@nodenebula.com>
2025-06-01 20:26:03 -0600

    expandable rows POC

--- src/client/modules/mint/components/mint-data-table/mint-data-table.component.ts
+++ src/client/modules/mint/components/mint-data-table/mint-data-table.component.ts
@@ -38,8 +38,9 @@ export class MintDataTableComponent implements OnChanges {
 	@Input() public chart_settings!: NonNullableMintDatabaseSettings;
 	@Input() public loading!: boolean;
 
-	public displayed_columns = ['unit', 'amount', 'created_time'];
+	public displayed_columns = ['unit', 'amount', 'request', 'state', 'created_time'];
   	public data_source!: MatTableDataSource<MintMintQuote>;
+	public more_entity!: MintMintQuote | null;
 
 	constructor() {}
 
@@ -55,4 +56,39 @@ export class MintDataTableComponent implements OnChanges {
 			this.data_source.sort = this.sort;
 		});
 	}
+
+	public toggleMore(entity: MintMintQuote) {
+		this.more_entity = this.more_entity === entity ? null : entity;
+	}
 }
+
+
+// /* Shared Dependencies */
+// import { OrchardMintMintQuote, MintQuoteState, MintUnit } from "@shared/generated.types";
+
+// export class MintMintQuote implements OrchardMintMintQuote {
+
+// 	public id: string;
+// 	public amount: number;
+// 	public unit: MintUnit;
+// 	public request: string;
+// 	public state: MintQuoteState;
+// 	public request_lookup_id: string;
+// 	public pubkey: string;
+// 	public issued_time: number | null;
+// 	public created_time: number | null;
+// 	public paid_time: number | null;
+
+// 	constructor(mint_mint_quote: OrchardMintMintQuote) {
+//         this.id = mint_mint_quote.id;
+// 		this.amount = mint_mint_quote.amount;
+// 		this.unit = mint_mint_quote.unit;
+// 		this.request = mint_mint_quote.request;
+// 		this.state = mint_mint_quote.state;
+// 		this.request_lookup_id = mint_mint_quote.request_lookup_id;
+// 		this.pubkey = mint_mint_quote.pubkey;
+// 		this.issued_time = mint_mint_quote.issued_time ?? null;
+// 		this.created_time = mint_mint_quote.created_time ?? null;
+// 		this.paid_time = mint_mint_quote.paid_time ?? null;
+// 	}
+// }
