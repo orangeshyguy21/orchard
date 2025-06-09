6558a85400e90be141bd8ceb3329df491bf6b759
Shyguy <shyguy@nodenebula.com>
2025-06-09 12:34:09 -0600

    cute little note boxes

--- src/client/modules/mint/components/mint-data-ecash/mint-data-ecash.component.ts
+++ src/client/modules/mint/components/mint-data-ecash/mint-data-ecash.component.ts
@@ -1,5 +1,5 @@
 /* Core Dependencies */
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 /* Application Dependencies */
 import { MintProofGroup } from '@client/modules/mint/classes/mint-proof-group.class';
 import { MintPromiseGroup } from '@client/modules/mint/classes/mint-promise-group.class';
@@ -12,12 +12,45 @@ import { MintKeyset } from '@client/modules/mint/classes/mint-keyset.class';
 	styleUrl: './mint-data-ecash.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MintDataEcashComponent {
+export class MintDataEcashComponent implements OnInit {
 
 	@Input() public group!: MintProofGroup | MintPromiseGroup;
 	@Input() public keysets!: MintKeyset[];
 
-	public get group_keysets(): MintKeyset[] {
-		return this.keysets.filter(keyset => this.group.keyset_ids.includes(keyset.id));
+	public group_keysets: MintKeyset[] = [];
+	public group_amounts: { keyset: MintKeyset|undefined; notes: { amount: number; quantity: number }[] }[] = [];
+	// public group_amounts = [
+	// 	{
+	// 		keyset: 'keyset_1',
+	// 		notes: [
+	// 			{
+	// 				amount: 128,
+	// 				quantity: 1
+	// 			}
+	// 		]
+	// 	},
+	// 	{}
+	// ]
+
+	ngOnInit(): void {
+		// this.group_keysets = this.keysets.filter(keyset => this.group.keyset_ids.includes(keyset.id));
+		this.group_amounts = this.group.keyset_ids.map((keyset_id, index) => {
+			return {
+				keyset: this.keysets.find(keyset => keyset.id === keyset_id),
+				notes: this.countAmounts(this.group.amounts[index]) || []
+			}
+		});
+	}
+
+	private countAmounts(amounts: number[]): { amount: number; quantity: number }[] {
+		return Array
+		.from(
+			amounts.reduce((acc, amount) => {
+				acc.set(amount, (acc.get(amount) || 0) + 1);
+				return acc;
+			}, new Map<number, number>()
+		))
+		.map(([amount, quantity]) => ({ amount, quantity }))
+		.sort((a, b) => b.amount - a.amount);
 	}
 }
