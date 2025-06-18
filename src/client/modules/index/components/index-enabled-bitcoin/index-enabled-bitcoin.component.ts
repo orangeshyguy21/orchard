f49f9acfadc8551696af72acb034dbed02f2e2da
Shyguy <shyguy@nodenebula.com>
2025-06-17 22:05:08 -0600

    bitcoin needs ln and ta errors

--- src/client/modules/index/components/index-enabled-bitcoin/index-enabled-bitcoin.component.ts
+++ src/client/modules/index/components/index-enabled-bitcoin/index-enabled-bitcoin.component.ts
@@ -6,6 +6,7 @@ import { BitcoinBlockchainInfo } from '@client/modules/bitcoin/classes/bitcoin-b
 import { BitcoinNetworkInfo } from '@client/modules/bitcoin/classes/bitcoin-network-info.class';
 import { LightningAccount } from '@client/modules/lightning/classes/lightning-account.class';
 import { TaprootAssets } from '@client/modules/tapass/classes/taproot-assets.class';
+import { OrchardError } from '@client/modules/error/types/error.types';
 
 type HotCoins = {
 	unit: string;
@@ -40,6 +41,8 @@ export class IndexEnabledBitcoinComponent implements OnChanges {
 	@Input() network_info!: BitcoinNetworkInfo;
 	@Input() lightning_accounts!: LightningAccount[];
 	@Input() taproot_assets!: TaprootAssets;
+	@Input() errors_lightning!: OrchardError[];
+	@Input() errors_taproot_assets!: OrchardError[];
 
 	@Output() navigate: EventEmitter<void> = new EventEmitter<void>();
 
@@ -71,6 +74,7 @@ export class IndexEnabledBitcoinComponent implements OnChanges {
 
 	private getLightningWalletBalance(): number {
 		if( !this.enabled_lightning ) return 0;
+		if( this.errors_lightning?.length > 0 ) return 0;
 		return this.lightning_accounts
 			.flatMap(account => account.addresses)
 			.reduce((sum, address) => sum + address.balance, 0);
@@ -78,6 +82,7 @@ export class IndexEnabledBitcoinComponent implements OnChanges {
 
 	private getLightningWalletUtxos(): number {
 		if (!this.enabled_lightning) return 0;
+		if( this.errors_lightning?.length > 0 ) return 0;
 		const unique_addresses = new Set(
 			this.lightning_accounts
 				.flatMap(account => account.addresses)
@@ -88,6 +93,7 @@ export class IndexEnabledBitcoinComponent implements OnChanges {
 
 	private getTaprootAssetsWalletBalance(): HotCoins[] | null {
 		if (!this.enabled_taproot_assets) return null;
+		if( this.errors_taproot_assets?.length > 0 ) return null;
 		const grouped_assets = this.taproot_assets.assets.reduce((acc, asset) => {
 			const asset_id = asset.asset_genesis.asset_id;
 			const amount = parseInt(asset.amount) / Math.pow(10, asset.decimal_display?.decimal_display || 0);
