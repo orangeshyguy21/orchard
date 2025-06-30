d1178c495ed321c76551ba8abffda835cf5c008b
Shyguy <shyguy@nodenebula.com>
2025-06-30 13:31:10 -0600

    standard containers

--- src/client/modules/index/components/index-enabled-ecash/index-enabled-ecash.component.ts
+++ src/client/modules/index/components/index-enabled-ecash/index-enabled-ecash.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'orc-index-enabled-ecash',
-  standalone: false,
-  templateUrl: './index-enabled-ecash.component.html',
-  styleUrl: './index-enabled-ecash.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'orc-index-enabled-ecash',
+	standalone: false,
+	templateUrl: './index-enabled-ecash.component.html',
+	styleUrl: './index-enabled-ecash.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IndexEnabledEcashComponent {
 
