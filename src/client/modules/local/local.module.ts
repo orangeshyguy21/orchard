a04fd3cd197f282356415f7b94ac75c164326794
Shyguy <shyguy@nodenebula.com>
2025-07-22 11:20:09 -0600

    remove benign pipe

--- src/client/modules/local/local.module.ts
+++ src/client/modules/local/local.module.ts
@@ -4,7 +4,6 @@ import {CommonModule} from '@angular/common';
 /* Native Module Dependencies */
 import {TimePipe} from './pipes/time/time.pipe';
 import {AmountPipe} from './pipes/amount/amount.pipe';
-import {FeePipe} from './pipes/fee/fee.pipe';
 import {UnitPipe} from './pipes/unit/unit.pipe';
 import {TruncatePipe} from './pipes/truncate/truncate.pipe';
 import {TimeDeltaPipe} from './pipes/time-delta/time-delta.pipe';
@@ -12,8 +11,8 @@ import {TimeAgoPipe} from './pipes/time-ago/time-ago.pipe';
 import {BytesPipe} from './pipes/bytes/bytes.pipe';
 
 @NgModule({
-	declarations: [TimePipe, AmountPipe, FeePipe, UnitPipe, TruncatePipe, TimeDeltaPipe, TimeAgoPipe, BytesPipe],
+	declarations: [TimePipe, AmountPipe, UnitPipe, TruncatePipe, TimeDeltaPipe, TimeAgoPipe, BytesPipe],
 	imports: [CommonModule],
-	exports: [TimePipe, AmountPipe, FeePipe, UnitPipe, TruncatePipe, TimeDeltaPipe, TimeAgoPipe, BytesPipe],
+	exports: [TimePipe, AmountPipe, UnitPipe, TruncatePipe, TimeDeltaPipe, TimeAgoPipe, BytesPipe],
 })
 export class LocalModule {}
