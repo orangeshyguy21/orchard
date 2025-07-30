9b5438f1fb62c2895770fd6fc5db54a1a7d8063c
Shyguy <shyguy@nodenebula.com>
2025-07-30 11:43:00 -0600

    fix keyset fee estimation

--- src/client/modules/mint/components/mint-keyset-rotation/mint-keyset-rotation.component.ts
+++ src/client/modules/mint/components/mint-keyset-rotation/mint-keyset-rotation.component.ts
@@ -1,5 +1,5 @@
 /* Core Dependencies */
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, computed} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 /* Native Dependencies */
 import {MintKeyset} from '@client/modules/mint/classes/mint-keyset.class';
@@ -20,29 +20,6 @@ export class MintKeysetRotationComponent {
 	@Input() median_notes!: number;
 
 	@Output() close = new EventEmitter<void>();
-
-	// public fee_error = computed(() => {
-	// 	if (this.form_group.get('input_fee_ppk')?.hasError('required')) return 'Required';
-	// 	if (this.form_group.get('input_fee_ppk')?.hasError('min')) return `Must be at least ${this.form_group.get('input_fee_ppk')?.getError("min")?.min}`;
-	// 	if (this.form_group.get('input_fee_ppk')?.hasError('max')) return `Must be at most ${this.form_group.get('input_fee_ppk')?.getError("max")?.max}`;
-	// 	if (this.form_group.get('input_fee_ppk')?.errors) return 'Invalid';
-	// 	return '';
-	// });
-
-	// public max_order_error = computed(() => {
-	// 	if (this.form_group.get('max_order')?.hasError('required')) return 'Required';
-	// 	if (this.form_group.get('max_order')?.hasError('min')) return `Must be at least ${this.form_group.get('max_order')?.getError("min")?.min}`;
-	// 	if (this.form_group.get('max_order')?.hasError('max')) return `Must be at most ${this.form_group.get('max_order')?.getError("max")?.max}`;
-	// 	if (this.form_group.get('max_order')?.errors) return 'Invalid';
-	// 	return '';
-	// });
-
-	// public keyset_in_fee_estimate = computed(() => {
-	// 	const fee_ppk = this.form_group.get('input_fee_ppk')?.value;
-	// 	const median_notes = this.median_notes;
-	// 	console.log('MEDIAN NOTES', median_notes);
-	// 	return Math.floor((median_notes * fee_ppk + 999) / 1000);
-	// });
 
 	public get fee_error(): string {
 		if (this.form_group.get('input_fee_ppk')?.hasError('required')) return 'Required';
@@ -67,6 +44,7 @@ export class MintKeysetRotationComponent {
 	public get keyset_in_fee_estimate(): number {
 		const fee_ppk = this.form_group.get('input_fee_ppk')?.value;
 		const median_notes = this.median_notes;
+		if (fee_ppk > 0 && median_notes === 0) return 1;
 		return Math.floor((median_notes * fee_ppk + 999) / 1000);
 	}
 }
