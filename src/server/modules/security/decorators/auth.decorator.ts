37d80fb034dda5a70f8216ca5094740e7817af8b
Shyguy <shyguy@nodenebula.com>
2025-08-11 11:03:33 -0600

    noheaders guard

--- src/server/modules/security/decorators/auth.decorator.ts
+++ src/server/modules/security/decorators/auth.decorator.ts
@@ -2,3 +2,6 @@ import {SetMetadata} from '@nestjs/common';
 
 export const PUBLIC_KEY = 'public';
 export const Public = () => SetMetadata(PUBLIC_KEY, true);
+
+export const NO_HEADERS_KEY = 'no_headers';
+export const NoHeaders = () => SetMetadata(NO_HEADERS_KEY, true);
--- src/server/modules/security/guards/auth.guard.ts
+++ src/server/modules/security/guards/auth.guard.ts
@@ -9,6 +9,7 @@ import {OrchardErrorCode} from '@server/modules/error/error.types';
 import {OrchardApiError} from '@server/modules/graphql/classes/orchard-error.class';
 /* Native Dependencies */
 import {PUBLIC_KEY} from '@server/modules/security/decorators/auth.decorator';
+import {NO_HEADERS_KEY} from '@server/modules/security/decorators/auth.decorator';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
@@ -26,6 +27,8 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
 	canActivate(context: ExecutionContext) {
 		const is_public = this.reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [context.getHandler(), context.getClass()]);
 		if (is_public) return true;
+		const no_headers = this.reflector.getAllAndOverride<boolean>(NO_HEADERS_KEY, [context.getHandler(), context.getClass()]);
+		if (no_headers) return true;
 		const production = this.configService.get<boolean>('mode.production');
 		if (!production) return true;
 		return super.canActivate(context);
