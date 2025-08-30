517054059733b27e71f8b6c8323626efac1a19bb
Shyguy <shyguy@nodenebula.com>
2025-08-29 21:51:00 -0600

    throttle the endpoint

--- src/server/modules/api/bitcoin/oracle/btcoracle.resolver.ts
+++ src/server/modules/api/bitcoin/oracle/btcoracle.resolver.ts
@@ -1,6 +1,7 @@
 /* Core Dependencies */
 import {Logger} from '@nestjs/common';
 import {Resolver, Query, Args} from '@nestjs/graphql';
+import {Throttle, minutes} from '@nestjs/throttler';
 /* Local Dependencies */
 import {OrchardBitcoinOracle} from './btcoracle.model';
 import {BitcoinOracleService} from './btcoracle.service';
@@ -12,6 +13,7 @@ export class BitcoinOracleResolver {
 	constructor(private bitcoinOracleService: BitcoinOracleService) {}
 
 	@Query(() => OrchardBitcoinOracle)
+	@Throttle({default: {limit: 2, ttl: minutes(60)}})
 	async bitcoin_oracle_recent(
 		@Args('include_intraday', {type: () => Boolean, nullable: true}) include_intraday?: boolean,
 	): Promise<OrchardBitcoinOracle> {
@@ -21,6 +23,7 @@ export class BitcoinOracleResolver {
 	}
 
 	@Query(() => OrchardBitcoinOracle)
+	@Throttle({default: {limit: 2, ttl: minutes(60)}})
 	async bitcoin_oracle_date(
 		@Args('date', {type: () => String}) date: string,
 		@Args('include_intraday', {type: () => Boolean, nullable: true}) include_intraday?: boolean,
