48b06e240bb829be8b977e5876895e11b0c5bfdb
Shyguy <orangeshyguy@pm.me>
2025-09-26 16:47:55 -0600

    tertiary nav refactor

--- src/client/modules/nav/components/tertiary-nav/tertiary-nav.component.ts
+++ src/client/modules/nav/components/tertiary-nav/tertiary-nav.component.ts
@@ -1,6 +1,5 @@
 /* Core Dependencies */
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, Output, EventEmitter} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 /* Vendor Dependencies */
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 /* Native Dependencies */
@@ -12,21 +11,6 @@ import {TertiaryNavItem} from '@client/modules/nav/types/tertiary-nav-item.type'
 	templateUrl: './tertiary-nav.component.html',
 	styleUrl: './tertiary-nav.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	// prettier-ignore
-	animations: [
-		trigger('orderUpdateAnimation', [
-			state('visible', style({
-				opacity: 1,
-				transform: 'translateY(0)'
-			})),
-			state('hidden', style({
-				opacity: 0,
-				transform: 'translateY(-0.5rem)',
-			})),
-			transition('hidden => visible', animate('100ms ease-out')),
-			transition('visible => hidden', animate('100ms ease-in', style({ opacity: 0 }))),
-		]),
-	],
 })
 export class TertiaryNavComponent {
 	@Input() keys: string[] = [];
@@ -36,7 +20,7 @@ export class TertiaryNavComponent {
 	@Output() orderChange = new EventEmitter<string[]>();
 	@Output() keySelect = new EventEmitter<string>();
 
-	public order_update_animation_state: 'visible' | 'hidden' = 'hidden';
+	public update_feedback: boolean = false;
 	private order_update_timeout: any;
 
 	constructor(private cdr: ChangeDetectorRef) {}
@@ -54,10 +38,10 @@ export class TertiaryNavComponent {
 
 	public showUpdateMessage(): void {
 		if (this.order_update_timeout) clearTimeout(this.order_update_timeout);
-		this.order_update_animation_state = 'visible';
+		this.update_feedback = true;
 		this.cdr.detectChanges();
 		this.order_update_timeout = setTimeout(() => {
-			this.order_update_animation_state = 'hidden';
+			this.update_feedback = false;
 			this.cdr.detectChanges();
 		}, 1000);
 	}
