1ba6288571cd407f464b5b78210f29520380894d
Shyguy <orangeshyguy@pm.me>
2025-09-29 17:23:15 -0600

    an example of a working client test

--- src/client/modules/mint/components/mint-keyset-table/mint-keyset-table.component.spec.ts
+++ src/client/modules/mint/components/mint-keyset-table/mint-keyset-table.component.spec.ts
@@ -1,5 +1,16 @@
+/* Core Dependencies */
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+/* Vendor Dependencies */
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatIconModule} from '@angular/material/icon';
+import {MatNativeDateModule} from '@angular/material/core';
+/* Application Dependencies */
+import {GraphicModule} from '@client/modules/graphic/graphic.module';
+import {LocalModule} from '@client/modules/local/local.module';
+import {MintModule} from '@client/modules/mint/mint.module';
+/* Local Dependencies */
 import {MintKeysetTableComponent} from './mint-keyset-table.component';
 
 describe('MintKeysetTableComponent', () => {
@@ -9,6 +20,18 @@ describe('MintKeysetTableComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [MintKeysetTableComponent],
+			imports: [
+				// Angular Material used in the template
+				MatCardModule,
+				MatTableModule,
+				MatSortModule,
+				MatIconModule,
+				MatNativeDateModule,
+				// Feature modules for custom components and pipes
+				GraphicModule,
+				MintModule,
+				LocalModule,
+			],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(MintKeysetTableComponent);
