ac8a6c909afa2404d994ca424b84cb6bfd04b3f4
Shyguy <orangeshyguy@pm.me>
2025-10-08 14:11:23 -0600

    fix tests p.2

--- src/client/modules/bitcoin/modules/bitcoin-subsection-dashboard/components/bitcoin-subsection-dashboard/bitcoin-subsection-dashboard.component.spec.ts
+++ src/client/modules/bitcoin/modules/bitcoin-subsection-dashboard/components/bitcoin-subsection-dashboard/bitcoin-subsection-dashboard.component.spec.ts
@@ -1,5 +1,10 @@
+/* Core Dependencies */
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+/* Vendor Dependencies */
+import {MatIconTestingModule} from '@angular/material/icon/testing';
+/* Native Dependencies */
+import {OrcBitcoinSubsectionDashboardModule} from '@client/modules/bitcoin/modules/bitcoin-subsection-dashboard/bitcoin-subsection-dashboard.module';
+/* Local Dependencies */
 import {BitcoinSubsectionDashboardComponent} from './bitcoin-subsection-dashboard.component';
 
 describe('BitcoinSubsectionDashboardComponent', () => {
@@ -8,7 +13,7 @@ describe('BitcoinSubsectionDashboardComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [BitcoinSubsectionDashboardComponent],
+			imports: [MatIconTestingModule, OrcBitcoinSubsectionDashboardModule],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(BitcoinSubsectionDashboardComponent);
