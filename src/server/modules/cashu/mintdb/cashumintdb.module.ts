04b113865f788b5a0088789dee8d4e3743fdab34
Shyguy <orangeshyguy@pm.me>
2025-10-14 17:41:24 -0600

    Fix Postgres Connection Strings wtih sslmode
    This adds a happy path for postgres mint databases using sslmode

--- src/server/modules/cashu/mintdb/cashumintdb.module.ts
+++ src/server/modules/cashu/mintdb/cashumintdb.module.ts
@@ -1,13 +1,14 @@
 /* Core Dependencies */
 import {Module} from '@nestjs/common';
 /* Application Dependencies */
+import {CredentialModule} from '@server/modules/credential/credential.module';
 import {NutshellModule} from '@server/modules/cashu/nutshell/nutshell.module';
 import {CdkModule} from '@server/modules/cashu/cdk/cdk.module';
 /* Local Dependencies */
 import {CashuMintDatabaseService} from './cashumintdb.service';
 
 @Module({
-	imports: [NutshellModule, CdkModule],
+	imports: [CredentialModule, NutshellModule, CdkModule],
 	providers: [CashuMintDatabaseService],
 	exports: [CashuMintDatabaseService],
 })
