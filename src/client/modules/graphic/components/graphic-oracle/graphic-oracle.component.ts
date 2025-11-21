f61964817485daa3973b3de43a9049df564341a0
Shyguy <orangeshyguy@pm.me>
2025-11-21 10:00:12 -0700

    error state oracle

--- src/client/modules/graphic/components/graphic-oracle/graphic-oracle.component.ts
+++ src/client/modules/graphic/components/graphic-oracle/graphic-oracle.component.ts
@@ -1,5 +1,7 @@
 /* Core Dependencies */
 import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+/* Shared Dependencies */
+import {UtxOracleProgressStatus} from '@shared/generated.types';
 
 @Component({
 	selector: 'orc-graphic-oracle',
@@ -12,5 +14,7 @@ export class GraphicOracleComponent {
 	public surface = input<boolean>(false);
 	public height = input.required<string>();
 	public running = input.required<boolean>();
-	public completed = input.required<boolean>();
+	public status = input.required<UtxOracleProgressStatus | null>();
+
+	public UtxOracleProgressStatus = UtxOracleProgressStatus;
 }
