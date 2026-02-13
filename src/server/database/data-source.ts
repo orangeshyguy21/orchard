d1391f1ca5032f93f159a077db30894efb0f4e13
Shyguy <orangeshyguy@pm.me>
2026-02-13 09:39:14 -0700

    chore: db migration

--- src/server/database/data-source.ts
+++ src/server/database/data-source.ts
@@ -6,11 +6,13 @@ import {Invite} from '../modules/invite/invite.entity';
 import {TokenBlacklist} from '../modules/auth/token-blacklist.entity';
 import {Setting} from '../modules/setting/setting.entity';
 import {UTXOracle} from '../modules/bitcoin/utxoracle/utxoracle.entity';
+import {AnalyticsCheckpoint} from '../modules/analytics/analytics-checkpoint.entity';
+import {LightningAnalytics} from '../modules/lightning/analytics/lnanalytics.entity';
 
 export const AppDataSource = new DataSource({
 	type: 'better-sqlite3',
 	database: process.env.DATABASE_DIR ? `${process.env.DATABASE_DIR}/orchard.db` : 'data/orchard.db',
-	entities: [User, Invite, TokenBlacklist, Setting, UTXOracle],
+	entities: [User, Invite, TokenBlacklist, Setting, UTXOracle, AnalyticsCheckpoint, LightningAnalytics],
 	migrations: ['src/server/database/migrations/*.ts'],
 	synchronize: false,
 });
