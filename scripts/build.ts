e81c245a76c0dfcd1b3d2679c4a24b12645e402e
Shyguy <orangeshyguy@pm.me>
2026-04-01 23:14:39 -0600

    fix: better build copy

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 
 const steps = [
     { label: 'Generating Graph API schema', cmd: 'npx ts-node -r tsconfig-paths/register --project tsconfig.server.json scripts/generate-schema.ts' },
-    { label: 'Generating TypeScript types', cmd: 'npx graphql-codegen' },
+    { label: 'Generating Shared types', cmd: 'npx graphql-codegen' },
     { label: 'Building Server', cmd: 'nest build' },
     { label: 'Building Client', cmd: 'ng build' },
 ];
